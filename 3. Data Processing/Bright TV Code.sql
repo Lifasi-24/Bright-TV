5a49e64f2d06d2a4c68a1b763472b1a96a74f864
lifasiqueenm@gmail.com <lifasiqueenm@gmail.com>
2026-07-15 11:57:01 +0000

    UserID Checks

--- 3. Data Processing/Bright TV Code.sql
+++ 3. Data Processing/Bright TV Code.sql
@@ -8,3 +8,18 @@ FROM user_profiles;
 
 SELECT *
 FROM viewership;
+
+-- checking for duplicates in my data 
+SELECT UserID,  
+COUNT(*) AS duplicate_count 
+FROM user_profiles 
+GROUP BY UserID 
+HAVING COUNT(*) > 1; 
+
+-- Are the any rows where userID is NULL  
+SELECT COUNT(*) AS cnt 
+FROM user_profiles 
+WHERE UserID IS NULL; 
+
+SELECT DISTINCT UserID 
+FROM user_profiles; 
