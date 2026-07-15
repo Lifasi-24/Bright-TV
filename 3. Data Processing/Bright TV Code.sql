6209cf27d1876f02a637af1dfe1198df6ecb6c41
lifasiqueenm@gmail.com <lifasiqueenm@gmail.com>
2026-07-15 12:15:18 +0000

    Created user_profiles as a CTE

--- 3. Data Processing/Bright TV Code.sql
+++ 3. Data Processing/Bright TV Code.sql
@@ -68,10 +68,53 @@ FROM user_profiles;
 
 ---------------------Age Checks------------------------- 
  SELECT MIN(Age) AS min_age,
-        MAX(Age) AS max_age,
-        AVG(Age) AS avg_age
+        MAX(Age) AS max_age
  FROM user_profiles; 
 
  SELECT COUNT(*) AS cnt 
  FROM user_profileS 
  WHERE age IS NULL; 
+
+ WITH user_profiles AS (---user_profiles is a CTE
+SELECT UserID, 
+ 
+    CASE  
+        WHEN Province=' ' THEN 'Uncategorized' 
+        WHEN Province='None' THEN 'Uncategorized' 
+    ELSE Province 
+    END AS Location,
+
+   age, 
+    CASE 
+        WHEN age = 0 THEN 'Infants' 
+        WHEN age BETWEEN 1 AND 12 THEN 'Kids' 
+        WHEN age BETWEEN 13 AND 19 THEN 'Teenager' 
+        WHEN age BETWEEN 20 AND 35 THEN 'Youth' 
+        WHEN age BETWEEN 36 AND 50 THEN 'Adult' 
+        WHEN age BETWEEN 51 AND 65 THEN 'Elder' 
+        WHEN age >65 THEN 'Pensioner' 
+    END AS age_groups, 
+
+    CASE 
+        WHEN (email IS NOT NULL ) OR (email=' ') OR (email NOT IN ('None'))THEN 1 
+    ELSE 0 
+    END AS email_flag, 
+
+    CASE 
+        WHEN `Social Media Handle` IS NOT NULL OR `Social Media Handle`=' ' OR  `Social Media Handle` NOT IN ('None') THEN 1 
+    ELSE 0 
+    END AS sm_flag, 
+
+    CASE 
+        WHEN Race='other' THEN 'None' 
+        WHEN Race=' ' THEN 'None' 
+    ELSE Race 
+    END AS Race, 
+ 
+    CASE 
+        WHEN gender =' ' THEN 'None' 
+        ELSE gender 
+    END AS Gender
+
+FROM user_profiles 
+), 
